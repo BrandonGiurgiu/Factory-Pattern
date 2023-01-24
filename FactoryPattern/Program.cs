ee169ab6980cd484403f2329f1f394d58c9f11fb
bgiurgiu7@gmail.com <bgiurgiu7@gmail.com>
2023-01-24 02:04:11 -0700

    Completed Exercise 1

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the amount of tires for the vehicle you want to create");
+
+            var userInput = Console.ReadLine();
+
+            var newBuild = VehicleFactory.GetVehicle(userInput);
+            newBuild.Drive();
+
+            Console.ReadLine();
+
+
+             
         }
     }
 }
+
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace FactoryPattern
+{
+	public class VehicleFactory
+	{
+		public VehicleFactory()
+		{
+		}
+
+		public static IVehicle GetVehicle(string wheelCount)
+		{
+            switch (wheelCount)
+			{
+				case "2":
+					return new Motorcycle();
+				case "4":
+					return new Car();
+				default:
+					return new Car();
+			}
+
+		}
+
+	}
+}
+
